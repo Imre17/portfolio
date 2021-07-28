2f856330c5e31685238ef6984147c83a902389a1
Imre <imreboloni17@gmail.com>
2021-07-28 09:05:41 +0100

    lower case

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './COMPONENTS/about/about.component';
-import { ContactMeComponent } from './COMPONENTS/contact-me/contact-me.component';
-import { HomeComponent } from './COMPONENTS/home/home.component';
-import { AudiophileComponent } from './COMPONENTS/projects/audiophile/audiophile.component';
-import { EasybankComponent } from './COMPONENTS/projects/easybank/easybank.component';
-import { LoopstodioComponent } from './COMPONENTS/projects/loopstodio/loopstodio.component';
-import { MovieAppComponent } from './COMPONENTS/projects/movie-app/movie-app.component';
-import { ProjectsComponent } from './COMPONENTS/projects/projects.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
+import { HomeComponent } from './components/home/home.component';
+import { AudiophileComponent } from './components/projects/audiophile/audiophile.component';
+import { EasybankComponent } from './components/projects/easybank/easybank.component';
+import { LoopstodioComponent } from './components/projects/loopstodio/loopstodio.component';
+import { MovieAppComponent } from './components/projects/movie-app/movie-app.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './COMPONENTS/Shared/nav-bar/nav-bar.component';
-import { FooterComponent } from './COMPONENTS/Shared/footer/footer.component';
-import { HomeComponent } from './COMPONENTS/home/home.component';
-import { AboutComponent } from './COMPONENTS/about/about.component';
-import { ProjectsComponent } from './COMPONENTS/projects/projects.component';
-import { EasybankComponent } from './COMPONENTS/projects/easybank/easybank.component';
-import { LoopstodioComponent } from './COMPONENTS/projects/loopstodio/loopstodio.component';
-import { AudiophileComponent } from './COMPONENTS/projects/audiophile/audiophile.component';
-import { ContactMeComponent } from './COMPONENTS/contact-me/contact-me.component';
-import { MovieAppComponent } from './COMPONENTS/projects/movie-app/movie-app.component';
+import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { EasybankComponent } from './components/projects/easybank/easybank.component';
+import { LoopstodioComponent } from './components/projects/loopstodio/loopstodio.component';
+import { AudiophileComponent } from './components/projects/audiophile/audiophile.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
+import { MovieAppComponent } from './components/projects/movie-app/movie-app.component';
 
 @NgModule({
   declarations: [
